761891acafbe9150edc5b2eed8efb69f468000e5
Matt Miller <matt.f.miller@live.com>
2018-06-22 12:09:44 -0700

    Refactor and complete/pass tests for sql2oNoteDao.

--- src/main/java/dao/NoteDao.java
+++ src/main/java/dao/NoteDao.java
@@ -5,13 +5,12 @@ import models.Note;
 import java.util.List;
 
 public interface NoteDao {
-    // LIST
-    List<Note> getAllByReleaseId(int id);
-    Note findById(int id);
-
     // CREATE
     void add(Note note);
 
+    // READ
+    List<Note> getAllByReleaseId(int id);
+    Note findById(int id);
 
     // UPDATE
     void update(int id, String content);
